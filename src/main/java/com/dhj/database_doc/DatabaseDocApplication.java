e5378dd922afb9e85bbec05994efb987993ec1a4
郭明德 <gmd111389.jx@chinatelecom.cn>
2022-09-21 16:22:56 +0800

    init

---
+++ src/main/java/com/dhj/database_doc/DatabaseDocApplication.java
@@ -0,0 +1,13 @@
+package com.dhj.database_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhj/database_doc/DatabaseDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhj.database_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
