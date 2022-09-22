4ef82eae63e90d808afec3be2c2f9afdd971fa2e
郭明德 <gmd111389.jx@chinatelecom.cn>
2022-09-22 19:21:55 +0800

    程序优化+1

--- src/main/java/com/dhj/controller/DownloadController.java
+++ src/main/java/com/dhj/controller/DownloadController.java
@@ -3,24 +3,16 @@ package com.dhj.controller;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.StrUtil;
-import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.support.ExcelTypeEnum;
-import com.alibaba.excel.write.metadata.WriteSheet;
-import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.dhj.core.DealWordHandler;
-import com.dhj.service.ITableInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStreamWriter;
+import java.net.URLEncoder;
 import java.util.Date;
 
 import static com.dhj.config.ConfigConstants.*;
@@ -76,17 +68,17 @@ public class DownloadController {
             fileName = PROJECT_NAME + "数据库设计文档_" + DateUtil.format(new Date(), "HHmmss");
         }
 
-        ServletOutputStream outputStream = response.getOutputStream();
-        OutputStreamWriter utf8Writer = IoUtil.getUtf8Writer(outputStream);
+        // 通知浏览器以附件的形式下载处理，设置返回头要注意文件名有中文
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8") + SUFFIX);
+
+        OutputStreamWriter utf8Writer = IoUtil.getUtf8Writer(response.getOutputStream());
 
         // 写入文件数据
         dealWordHandler.writeWordData(utf8Writer);
 
-        // 通知浏览器以附件的形式下载处理，设置返回头要注意文件名有中文
-        response.setHeader("Content-disposition", "attachment;filename=" + new String(fileName.getBytes("gb2312"), "ISO8859-1") + SUFFIX);
-        response.setContentType("multipart/form-data");
-        response.setCharacterEncoding("utf-8");
-
+        utf8Writer.flush();
         utf8Writer.close();
     }
 
